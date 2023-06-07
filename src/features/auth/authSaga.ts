01b78245f4ddbbc12592040affe77a4c39ecadac
Thienluc <nguyenthienluc2004@gmail.com>
2023-06-08 00:20:30 +0700

    fix auth features

--- src/features/auth/authSaga.ts
+++ src/features/auth/authSaga.ts
@@ -1,37 +1,35 @@
-import { PayloadAction } from "@reduxjs/toolkit";
-import authApi from "api/authApi";
-import { Login, LoginResponse, Register, RegisterResponse } from "models";
-import { call, put, takeLatest } from "redux-saga/effects";
-import { authAction } from "./authSlice";
-import { toast } from "react-toastify";
-import { push } from "redux-first-history";
-import { status } from "utils/constant";
+import { PayloadAction } from '@reduxjs/toolkit';
+import authApi from 'api/authApi';
+import { CurrentUser, Login, Register } from 'models';
+import { toast } from 'react-toastify';
+import { push } from 'redux-first-history';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import { status } from 'utils/constant';
+import { authAction } from './authSlice';
 
 function* handleAuthLogin(action: PayloadAction<Login>) {
-  const response: LoginResponse = yield call(authApi.login, action.payload);
-  if (response.data?.username) {
-    yield put(authAction.authLoginSuccess(response.data));
-    yield put(push("/"));
-    toast.success("Login success");
+  const response: CurrentUser = yield call(authApi.login, action.payload);
+  console.log('function*handleAuthLogin ~ response:', response);
+  if (response?.username) {
+    yield put(authAction.authLoginSuccess(response));
+    yield put(push('/'));
+    toast.success('Login success');
   } else {
     yield put(authAction.authLoginFailed());
-    toast.error("Username or password maybe wrong");
+    toast.error('Username or password maybe wrong');
   }
 }
 
 function* handleAuthRegister(action: PayloadAction<Register>) {
-  const response: RegisterResponse = yield call(
-    authApi.register,
-    action.payload
-  );
-  if (response.data === status.OK) {
+  const response: number = yield call(authApi.register, action.payload);
+  if (response === status.OK) {
     yield put(authAction.authRegisterSuccess);
-    yield put(push("/login"));
-    toast.success("Your account has just been created");
-  } else if (response.data === status.EXIST) {
+    yield put(push('/login'));
+    toast.success('Your account has just been created');
+  } else if (response === status.EXIST) {
     yield put(authAction.authRegisterFailed);
-    toast.error("Your account already exists");
-  } else if (response.data === status.ERROR) {
+    toast.error('Your account already exists');
+  } else if (response === status.ERROR) {
     yield put(authAction.authRegisterFailed);
     toast.error("Can't create your account, please enter again");
   }
